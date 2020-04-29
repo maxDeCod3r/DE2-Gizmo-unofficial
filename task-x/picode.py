d16b55073ee6df01978adb16f23d492fb4c5b5de
Max Hunt <mh2817@ic.ac.uk>
2020-04-29 09:47:00 +0100

    Update

--- task-x/picode.py
+++ task-x/picode.py
@@ -1,23 +1,8 @@
 from gpiozero import Button, PWMLED, LED
 from time import sleep
 
-pled = PWMLED(4)
-led = LED(3)
-button = Button(2)
-litt = True
-pled.value = 1
-led.off
-
-while True:
-	if button.is_pressed and litt:
-		fadetoL()
-		blickOnce()
-	elif not litt and not button.is_pressed:
-		fadetoH()
-
-
-
 def fadetoL(curVal = 1):
+	global litt
 	for i in range(75):
 		curVal-=0.01
 		pled.value = curVal
@@ -25,6 +10,7 @@ def fadetoL(curVal = 1):
 	litt = False
 
 def fadetoH(curVal = 0.25):
+	global litt
 	for i in range(75):
 		curVal+=0.01
 		pled.value = curVal
@@ -35,3 +21,19 @@ def blickOnce():
 	led.on()
 	sleep(0.3)
 	led.off()
+
+pled = PWMLED(4)
+led = LED(3)
+button = Button(2)
+litt = True
+pled.value = 1
+led.off()
+
+while True:
+	if button.is_pressed and litt:
+		fadetoL()
+		blickOnce()
+	elif not litt and not button.is_pressed:
+		fadetoH()
+
+
